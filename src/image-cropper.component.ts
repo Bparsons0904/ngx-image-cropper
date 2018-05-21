00ca8ea12e1ffcc1deca9cab7243b80082d75e65
Bob Parsons <Deadstylebp@gmail.com>
2018-05-21 18:40:02 -0500

    Update image-cropper.component.ts

--- src/image-cropper.component.ts
+++ src/image-cropper.component.ts
@@ -156,6 +156,34 @@ export class ImageCropperComponent implements OnChanges {
             });
         }
     }
+    
+    @HostListenter('click')
+    rotateImage(): void {
+        // create an off-screen canvas
+        var offScreenCanvas = document.createElement('canvas');
+        var offScreenCanvasCtx = offScreenCanvas.getContext('2d');
+
+        // load current image
+        var image = document.getElementsByClassName("source-image");
+        var base64Image = image[0].currentSrc;
+
+        // create Image
+        var img = new Image();
+        img.src = base64Image;
+
+        // set its dimension to rotated size
+        offScreenCanvas.height = img.width;
+        offScreenCanvas.width = img.height;
+
+        // rotate and draw source image into the off-screen canvas:
+        offScreenCanvasCtx.rotate(90 * Math.PI / 180);
+        offScreenCanvasCtx.translate(0, -offScreenCanvas.width);
+        offScreenCanvasCtx.drawImage(img, 0, 0);
+
+        // encode image to data-uri with base64
+        this.loadBase64Image(offScreenCanvas.toDataURL("image/jpeg", 100))
+    }
+                                         
 
     @HostListener('window:resize', ['$event'])
     imageResizedInView(event: Event) {
